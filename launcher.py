c57ad9464a9f56c8932ecee7dd3f4e0120606f2a
John Anthony <askinganthony@gmail.com>
2015-11-03 19:03:35 -0800

    Started to create the next level up on the program, which will split the amount you want to be tested into a managable group of 100.

--- launcher.py
+++ launcher.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 from datetime import datetime
 from multiprocessing import Process, Queue
+import matplotlib.pyplot as plt
 
 def configValue(fileName, key, returnType='str'):
 	configFile = open(fileName, 'r')
@@ -51,6 +52,8 @@ def testGroupSize(array, alg):
 	return groupSize(len(array), alg)
 
 def chooseApplicants(array, testGroup):
+	if testGroup==0:
+		return 0
 	highest = max(array[0:testGroup])
 	#print("Testing first " + str(len(array[0:testGroup])))
 	for x in array[testGroup-1:len(array)+1]:
@@ -94,10 +97,10 @@ def calculate(testCount, configFile, applicantCount, q, progress='no', customSiz
 
 
 ###BEGIN MAIN PROGRAM
-def testAccuracy(configFile, testGroupSize, applicantCount):
+def testAccuracy(configFile, testGroupSize, applicantCount, verbose='yes'):
 	testCount = configValue(configFile, "testCount", 'int')
 	quadMode = configValue(configFile, "quadMode")
-	print(configValue(configFile, "topRange"))
+	#print(configValue(configFile, "topRange"))
 	
 
 	correct=0.00
@@ -107,9 +110,9 @@ def testAccuracy(configFile, testGroupSize, applicantCount):
 	if __name__ == '__main__':    
 		results = []
 		if quadMode=='yes':
-		        print("Quad process mode...")
+
 		        threads=4
-		        p1 = Process(target=calculate, args=(testCount/threads, configFile, applicantCount, q, 'yes', 'yes', testGroupSize))
+		        p1 = Process(target=calculate, args=(testCount/threads, configFile, applicantCount, q, verbose, 'yes', testGroupSize))
 		        p1.start()
 		        p2 = Process(target=calculate, args=(testCount/threads, configFile, applicantCount, q, 'no', 'yes', testGroupSize))
 		        p2.start()
@@ -126,26 +129,65 @@ def testAccuracy(configFile, testGroupSize, applicantCount):
 		        
 		        
 		else:
-		        print("Single process mode...")
 		        threads=1
 		        p1 = Process(target=calculate, args=(testCount/threads, configFile, applicantCount, q, 'yes', 'yes', testGroupSize))
 		        p1.start() 
-
+			
 		        for i in range(threads):
 		                #set block=True to block until we get a result
 		                results.append(q.get(True))
 		                     
 	
 		correct=float(sum(results))
+		if verbose=='yes':
+			sys.stdout.write("\n")
+			#print(correct)
+			
+			#print(str(accuracy) + "% accuracy") 
+			#print("Operation took "+ str(datetime.now() - startTime))
 
-		sys.stdout.write("\n")
-		#print(correct)
-		print(str((correct/testCount)*100) + "% accuracy") 
-		print("Operation took "+ str(datetime.now() - startTime))
+		accuracy=(correct/testCount)*100
+		return accuracy
 
-testAccuracy("Secretary.cfg", 375, 1000)
+def hundreths_arr(value):
+	return_value=[]
+	divisor=value/100
+	for x in range(100):
+		return_value.append( int((x+1)*divisor) )
+	return return_value
+		
+		
 
+def findOptimalStopping(secretaryCount, method):
+	startTime = datetime.now()
+	values=[]
+	accuracy=[]
+	if method=='brute':
+		testers_hundreths=hundreths_arr(secretaryCount)
+		print(str(testers_hundreths))
+		for x in testers_hundreths:
+			indexAccuracy = testAccuracy("Secretary.cfg", x, secretaryCount)
+			accuracy.append(indexAccuracy)
+			values.append(x)
+			print("\n" +str(x)+" of "+str(secretaryCount/100) + " had accuracy of " + str(indexAccuracy))
+			progressBar(x, secretaryCount)
+			print("\n")
+	
+	
+	accuracy
+	accuracy_np=np.array(accuracy)
+	optimal_index=accuracy_np.argmax()
+	optimal_value=values[optimal_index]
 
+	print("\nOperation took "+ str(datetime.now() - startTime))
+	print("Optimal stopping point is: " + str(optimal_value))
+	
+	plt.plot(values, accuracy, 'ro')
+	plt.show()
+			
+aV=configValue("Secretary.cfg", "applicantCount", 'int')
+print(str(aV))
+findOptimalStopping(aV, 'brute')
 
 
 
